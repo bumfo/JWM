f2963bb4ab1b2b5a742d9bb8e4d050b42e418c05
bumfo <bumfo@users.noreply.github.com>
2022-04-07 00:20:04 +0800

    First working example

--- macos/cc/ClipboardMac.mm
+++ macos/cc/ClipboardMac.mm
@@ -1,10 +1,50 @@
 #include <jni.h>
 #include <AppKit/NSPasteboard.h>
+#import <Foundation/Foundation.h>
 #include "ClipboardMac.hh"
+#include <StringUTF16.hh>
+#include <impl/Library.hh>
+#include <impl/JNILocal.hh>
+#include <cstring>
 
 extern "C" JNIEXPORT void JNICALL Java_io_github_humbleui_jwm_Clipboard__1nSet
         (JNIEnv *env, jclass jclass, jobjectArray entries) {
-    printf("set\n");
+    NSLog(@"Clipboard.set");
+
+    using namespace jwm;
+
+    jsize size = env->GetArrayLength(entries);
+    for (jsize i = 0; i < size; ++i) {
+        jobject entry = env->GetObjectArrayElement(entries, i);
+
+        if (entry) {
+            jobject format = classes::ClipboardEntry::getFormat(env, entry);
+            jbyteArray data = classes::ClipboardEntry::getData(env, entry);
+            jsize dataSize = env->GetArrayLength(data);
+
+            StringUTF16 formatId = StringUTF16::makeFromJString(env, classes::ClipboardFormat::getFormatId(env, format));
+            std::string formatStdStr = formatId.toAscii();
+
+            if (formatStdStr == "text/plain") {
+              NSData* nsData;
+              {
+                  jbyte* dataBytes = env->GetByteArrayElements(data, nullptr);
+                  nsData = [NSData dataWithBytes:dataBytes length:dataSize];
+                  env->ReleaseByteArrayElements(data, dataBytes, JNI_ABORT);
+              }
+
+              NSPasteboard* board = [NSPasteboard generalPasteboard];
+
+              [board declareTypes:[NSArray arrayWithObject:NSPasteboardTypeString] owner:nil];
+
+              BOOL ret = [[NSPasteboard generalPasteboard] setData:nsData forType:NSPasteboardTypeString];
+
+              NSLog(@"setString ret = %d", ret);
+            } else {
+              NSLog(@"setData not implemented for %s", formatStdStr.c_str());
+            }
+        }
+    }
 }
 
 extern "C" JNIEXPORT jobject JNICALL Java_io_github_humbleui_jwm_Clipboard__1nGet
